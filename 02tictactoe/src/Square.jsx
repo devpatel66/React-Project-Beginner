4a3315b4a05a6e365c6e46719450224eddba0b64
devpatel66 <devpatel16@outlook.ingit config --global user.email devpatel16@outlook.ingit config --global user.email devpatel16@outlook.in>
2024-02-11 15:43:46 +0530

    done some changes

--- 02tictactoe/src/Square.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-
-function Square({value,onSquareClick}) {
-  return (
-    <button  onClick={onSquareClick} className='square'>{value}</button>
-  )
-}
-
-export default Square
